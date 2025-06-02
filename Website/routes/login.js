3e5276bd4e7184ce23e0d74c60a2db7b5e1f4757
hbgda <dheaster123@gmail.com>
2025-06-02 18:21:55 +0100

    login page outline

---
+++ Website/routes/login.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", function(req, res) {
+    res.render("login", { 
+        title: "Login",
+    });
+});
+
+module.exports = {
+    router_path: "/login",
+    router
+};
